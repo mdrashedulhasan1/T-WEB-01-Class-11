a6751107b37fa6a9dabbe1012d5eda1d387a7240
Rashedul Hasan <Rashedul Hasan>
2022-02-21 23:54:08 +0600

    Start JavaScript

---
+++ index.js
@@ -0,0 +1,51 @@
+// console.log("Hello World!");
+
+// var mangoPrice = 10.5454;
+// console.log(typeof mangoPrice);
+
+// var myName = "Tapu";
+// console.log(typeof myName);
+
+// var isGood = true;
+// console.log(typeof isGood);
+
+// var isBad = false;
+// console.log(typeof isBad);
+
+
+// var loveCountry = "I love my Country";
+// var update = loveCountry.toUpperCase();
+// var update2 = loveCountry.toLowerCase();
+// var pos = loveCountry.indexOf("Country");
+// console.log(update);
+// console.log(update2);
+// console.log(pos);
+
+
+// var num1 = 10;
+// var num2 = parseFloat("5.32453456");
+// var result = num1+num2;
+// console.log(result);
+
+// var num = 10;
+// var num2 = 3;
+// var add =  num+num2;
+// var sub =  num-num2;
+// var multi =  num*num2;
+// var div = num/num2;
+// var div = div.toFixed(2);
+// var rem = num%num2;
+// console.log("Add two number:",add);
+// console.log("Sub two number:",sub);
+// console.log("Multi two number:",multi);
+// console.log("Div two number:",div);
+// console.log("Rem two number:",rem);
+
+// var number = 10;
+// number++; //11 // number = number+1
+// number--; //10 // number = number-1
+// number++; //11
+// number++; //12
+// number++; //13
+// number--; //12
+// console.log(number);
